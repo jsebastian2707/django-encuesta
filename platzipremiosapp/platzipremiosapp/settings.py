9f56eaea2fd91c7c44c467c476a9b739bba643a2
sebastian <104945395+jsebastian2707@users.noreply.github.com>
2022-12-10 11:15:36 -0500

    secret_key delete

--- platzipremiosapp/platzipremiosapp/settings.py
+++ platzipremiosapp/platzipremiosapp/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s!*z^m#6wf)lpe-_+&1())%(saflt-2^ry7(^&sp_mpgs&#phw'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
